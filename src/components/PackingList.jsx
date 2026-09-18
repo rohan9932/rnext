2a73a6584a3f99cf03b913f084c4f8a5e40004ef
Rohan Rahim <rohanrahim024@gmail.com>
2026-09-18 12:06:44 +0600

    1.9 patch

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,26 +1,20 @@
 function Item({ name, isPacked }) {
-    let itemContent = "";
-
-    if (isPacked) {
-        itemContent = <span>{name + "✔"}</span>;
-    } else {
-        itemContent = <span>{name}</span>;
-    }
-
-    return <li className="item">{itemContent}</li>;
+  return (
+    <li className="item">{isPacked ? <del>{name + " ✓"}</del> : name}</li>
+  );
 }
 
 export default function PackingList() {
-    return (
-        <section>
-            <h1>Sally Rides Packing List</h1>
-            <ul>
-                <Item isPacked={true} name="Space suit" />
-                <Item isPacked={true} name="Helmet with a golden leaf" />
-                <Item isPacked={false} name="Photo of Tam" />
-            </ul>
-        </section>
-    );
+  return (
+    <section>
+      <h1>Sally Rides Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={false} name="Helmet with a golden leaf" />
+        <Item isPacked={true} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
 }
 
 // just for memory
