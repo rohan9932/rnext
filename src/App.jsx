f37fbddf0f7a27c2555c3b7efbf8076aa83dafe4
Rohan Rahim <rohanrahim024@gmail.com>
2026-09-23 08:51:17 +0600

    2.1 patch

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 export default function App() {
-    return (
-        <Toolbar
-            onPlayMovie={() => alert("Playing!")}
-            onUploadImage={() => alert("Uploading!")}
-        />
-    );
+  return (
+    <Toolbar
+      onPlayMovie={() => alert("Playing!")}
+      onUploadImage={() => alert("Uploading!")}
+    />
+  );
 }
 
 function Toolbar({ onPlayMovie, onUploadImage }) {
-    return (
-        <div>
-            <Button onSmash={onPlayMovie}>Play Movie</Button>
-            <Button onSmash={onUploadImage}>Upload Image</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button onSmash={onPlayMovie}>Play Movie</Button>
+      <Button onSmash={onUploadImage}>Upload Image</Button>
+    </div>
+  );
 }
 
 function Button({ onSmash, children }) {
-    return (
-        <p>
-            <button onClick={onSmash}>{children}</button>
-        </p>
-    );
+  return <button onClick={onSmash}>{children}</button>;
 }
