48eac18ca5916419831e36d7248f42064f71ef37
Rohan Rahim <rohanrahim024@gmail.com>
2026-09-23 09:30:51 +0600

    2.2 patches

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-export default function Signup() {
-    return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault();
-                alert("Submitting!");
-            }}
-        >
-            <input />
-            <button>Send</button>
-        </form>
-    );
+function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default function Toolbar() {
+  return (
+    <div
+      className="Toolbar"
+      onClick={() => {
+        alert("You clicked on the toolbar!");
+      }}
+    >
+      <Button onClick={() => alert("Playing!")}>Play Movie</Button>
+      <Button onClick={() => alert("Uploading!")}>Upload Image</Button>
+    </div>
+  );
 }
